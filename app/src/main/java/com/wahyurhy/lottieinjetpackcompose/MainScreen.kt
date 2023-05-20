8844a5fd9ba597c89b6520a445fda4a66afe4b26
wahyurhy <wahyurahayu82@gmail.com>
2023-05-20 10:18:56 +0700

    Initial commit

---
+++ app/src/main/java/com/wahyurhy/lottieinjetpackcompose/MainScreen.kt
@@ -0,0 +1,52 @@
+package com.wahyurhy.lottieinjetpackcompose
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+
+@Composable
+fun MainScreen() {
+
+    val composition by rememberLottieComposition(LottieCompositionSpec.RawRes(R.raw.happy_cat))
+    var isPlaying by remember { mutableStateOf(true) }
+
+    val progress by animateLottieCompositionAsState(
+        composition = composition,
+        isPlaying
+    )
+
+    LaunchedEffect(key1 = progress) {
+        if (progress == 0f) {
+            isPlaying = true
+        }
+        if (progress == 1f) {
+            isPlaying = false
+        }
+    }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        LottieAnimation(
+            modifier = Modifier.size(400.dp),
+            composition = composition,
+            progress = { progress }
+        )
+        Button(onClick = { isPlaying = true }) {
+            Text(text = "Play Again")
+        }
+    }
+}
